9c85976d77e3b80bf35f89b9ace89e7c9afd946f
cuonglm-1070 <le.manh.cuong@sun-asterisk.com>
2021-03-01 15:26:35 +0700

    add authorization

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::resource('posts', PostController::class);
+
+require __DIR__ . '/auth.php';
